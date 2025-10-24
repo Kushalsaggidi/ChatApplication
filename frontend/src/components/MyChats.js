a9f3fbc930ad3f15e7068e84d94dd093deb38832
Kushal <saggidikushal@gmail.com>
2025-10-24 23:33:20 +0530

    try2

--- frontend/src/components/MyChats.js
+++ frontend/src/components/MyChats.js
@@ -1,5 +1,5 @@
 import { AddIcon } from "@chakra-ui/icons";
-import { Box, Stack, Text } from "@chakra-ui/layout";
+import { Box, Stack, Text } from "@chakra-ui/react";
 import { useToast } from "@chakra-ui/toast";
 import axios from "axios";
 import { useEffect, useState } from "react";
